f7f3d64957cffabd6022cb536e8c49ff06172ccd
Yotam1312 <Yotam1312@gmail.com>
2026-03-26 18:10:16 +0200

    BASE URL Update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Change this to your server's IP address and port
 // Local dev example:  'http://192.168.1.100:8000'
 // Production example: 'https://your-app.azurewebsites.net'
-export const BASE_URL = 'http://192.168.68.105:8000';
+export const BASE_URL = 'https://taskup-api-hshmgkfcd9a6excz.israelcentral-01.azurewebsites.net';
